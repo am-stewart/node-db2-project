dfd0bb81ee5136d8a5dc5ce8f173b0b01eba8458
Allison Stewart <amstewart@yahoo.com>
2022-02-15 14:46:23 -0500

    created checkCarId middleware and get request for all cars

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,50 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model');
+const db = require('../../data/db-config');
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (!car) {
+      next({
+        status: 404,
+        message: `car with id ${req.params.id} is not found`
+      })
+    } else {
+      req.car = car
+      next();
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkCarPayload = async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberValid = async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,30 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll()
+        res.json(cars);
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router;
